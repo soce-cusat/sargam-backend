77f84803c5b59befc511a33dbe5b12826c3773e3
afnash <iamafnash@gmail.com>
2025-03-05 23:40:25 +0530

    profile added and pages configured

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,8 @@ from .views import *
 urlpatterns = [
 	path('register/', RegistrationView.as_view(), name="register"),
 	path('verify/<uidb64>/<token>/', verify_email, name='verify_email'),
+    path('login/',user_login, name="user_login"),
+    path('profile/',user_profile, name="user_profile"),
+
 
 ]
