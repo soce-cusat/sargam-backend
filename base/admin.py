9723f418ad29e4d6b5ce9e0f5c8b182ce3121f94
Answar C K <answarc764@gmail.com>
2025-03-08 09:36:42 +0530

    Filter added in admin panel and minor tweaks

--- base/admin.py
+++ base/admin.py
@@ -8,7 +8,7 @@ admin.site.register(IndividualItem)
 admin.site.register(GroupItem)
 
 class ParticipantAdmin(admin.ModelAdmin):
-    list_display = ('photo_display', 'name', 'email', 'zone', 'studentid')
+    list_display = ('photo_display', 'name', 'email', 'ph_number', 'zone', 'studentid')
     search_fields = ('name', 'email', 'studentid')
     list_filter = ('zone',)
     ordering = ('name',)
@@ -25,9 +25,12 @@ class ParticipantAdmin(admin.ModelAdmin):
                 ('Additional Info', {'fields': ('user', 'email', 'photo', 'id_card')}),
             )
 
-    def photo_display(self, obj):
-        return format_html('<img src="{}" width="50" height="50" style="border-radius:5px;" />', obj.photo.url)
+    def ph_number(self, obj):
+        return obj.ph_number
+    ph_number.short_description = "Phone Number"
 
+    def photo_display(self, obj):
+        return format_html('<img src="{}" width="50" height="50" style="border-radius:3px;" />', obj.photo.url)
     photo_display.short_description = 'Photo'
 
     def get_readonly_fields(self, request, obj=None):
@@ -49,6 +52,20 @@ class ZoneCaptainAdmin(admin.ModelAdmin):
 
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ['participant__name', 'item__item_name', 'status']
+    search_fields = ['participant__name', 'item__item_name']
+    list_editable = ['status']
+
+    def item__item_name(self, obj):
+        return obj.item.item_name
+    item__item_name.short_description = "Item"
+
+    def get_list_filter(self, request):
+        if request.user.is_superuser:
+            return ['participant', 'participant__zone']
+        else:
+            return ['participant']
+    
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
